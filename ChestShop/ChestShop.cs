0264dbe8499be933889261a5b0540f8fabf928ef
TheOldPlayeR <theoldplayer26@gmail.com>
2018-09-25 18:53:47 +0800

    added chest saving

--- ChestShop/ChestShop.cs
+++ ChestShop/ChestShop.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +14,17 @@ namespace ChestShop
         public List<ShopItem> ShopItems = new List<ShopItem>();
         public int ChestIndex;
         public Point Position;
+
+        public ChestShop(List<ShopItem> shopItems, int chestIndex, Point position)
+        {
+            ShopItems = shopItems;
+            ChestIndex = chestIndex;
+            Position = position;
+        }
+
+        public void SaveTo(string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
     }
 }
--- ChestShop/PluginMain.cs
+++ ChestShop/PluginMain.cs
@@ -1,29 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TShockAPI;
-using TerrariaApi.Server;
+using System.IO;
+using Microsoft.Xna.Framework;
 using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
 
 namespace ChestShop
 {
+    [ApiVersion(2, 1)]
     public class PluginMain : TerrariaPlugin
     {
         public PluginMain(Main game) : base(game)
         {
         }
 
-        public override string Name => "ChestShop";
+        public override string Name
+        {
+            get
+            {
+                return "ChestShop";
+            }
+        }
+        
+        public override Version Version
+        {
+            get
+            {
+                return new Version(1, 0);
+            }
+        }
 
-        public override Version Version => new Version(1,0);
+        public override string Author
+        {
+            get
+            {
+                return "AshtonB (aka TheOldPlayeR)";
+            }
+        }
 
-        public override string Author => "AshtonB (aka TheOldPlayeR)";
-
+        public bool[] isSelectingChest = new bool[255];
+        
         public override void Initialize()
         {
-            
+            for (int i = 0; i < 255; i++)
+            {
+                this.isSelectingChest[i] = false;
+            }
+            string filePath = Path.Combine(TShock.SavePath, "ChestShop");
+            bool flag = !Directory.Exists(filePath);
+            if (flag)
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            Commands.ChatCommands.Add(new Command("chestshop.manage", new CommandDelegate(this.ChestShopCommand), new string[]
+            {
+                "chestshop"
+            }));
+            GetDataHandlers.ChestOpen += new EventHandler<GetDataHandlers.ChestOpenEventArgs>(this.OnChestOpen);
+        }
+        
+        private void OnChestOpen(object sender, GetDataHandlers.ChestOpenEventArgs args)
+        {
+            bool flag = !this.isSelectingChest[args.Player.Index];
+            if (!flag)
+            {
+                int index = Chest.FindChest(args.X, args.Y);
+                ChestShop chestShop = new ChestShop(new List<ShopItem>(), index, new Point(args.X, args.Y));
+                chestShop.SaveTo(Path.Combine(TShock.SavePath, "ChestShop", string.Format("Chest_{0}.json", index)));
+            }
+        }
+        
+        private void ChestShopCommand(CommandArgs args)
+        {
+            TSPlayer p = args.Player;
+            bool selectingChest = this.isSelectingChest[p.Index];
+            bool flag = !selectingChest;
+            if (flag)
+            {
+                p.SendInfoMessage("Open a chest to save it's information to Chest_<id>.json, from which you can modify the shop.");
+                this.isSelectingChest[p.Index] = true;
+            }
+            else
+            {
+                bool flag2 = selectingChest;
+                if (flag2)
+                {
+                    p.SendInfoMessage("Canceled selection.");
+                    this.isSelectingChest[p.Index] = false;
+                }
+            }
         }
     }
 }
